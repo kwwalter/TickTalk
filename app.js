79f7c3e4fb514b3b25a4534e44b04e38b0421664
Kevin Walter <kevinwalter0731@gmail.com>
2015-11-17 20:22:05 -0500

    Working on angular basics

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-var app = angular.module = ('TickTalk', []);
+var app = angular.module('TickTalk', []);
 
-app.controller('MainController', ['$http', '$scope', ])
+app.controller('MainController', ['$http', '$scope', function($http) {
+  var controller = this;
+
+  this.test = "testing!";
+  this.posts = [
+    { title: 'post 1',
+      upvotes: 29
+    },
+    { title: 'post 2',
+      upvotes: 7
+    },
+    { title: 'post 3',
+      upvotes: 1
+    },
+    { title: 'post 4',
+      upvotes: 43
+    },
+    { title: 'post 5',
+      upvotes: 15
+    }
+  ];
+
+  this.addPost = function() {
+    controller.posts.push({
+      title: controller.title,
+      upvotes: 0
+    });
+    controller.title = ""; 
+  }
+
+}]);
