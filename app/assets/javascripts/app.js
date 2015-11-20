753d204402ade8731c5ddbf4274dc13090e25507
Kevin Walter <kevinwalter0731@gmail.com>
2015-11-19 22:47:53 -0500

    Moving forward despite the routing problems, trying to get comments fully functional (I hope)

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -23,47 +23,58 @@ app.controller('MainController', ['$http', '$scope', 'posts', function($http, $s
     // var tagsArray = $scope.tags.split(/, \s?/);
     // console.log("tags array is: ", tagsArray);
 
-    $scope.posts.push({
+    // won't be using this code now that we'll be saving to the database..
+
+    // $scope.posts.push({
+    //   title: $scope.title,
+    //   body: $scope.body,
+    //   // tags: tagsArray,
+    //   rating: 0
+    //   // comments: [
+    //   //   {
+    //   //     author: 'Kevin',
+    //   //     commentBody: 'Hella tight brah!',
+    //   //     rating: 0
+    //   //   },
+    //   //   {
+    //   //     author: 'Rick',
+    //   //     commentBody: 'Wubba lubba dub dub!',
+    //   //     rating: 0
+    //   //   },
+    //   //   {
+    //   //     author: 'Tiny Rick',
+    //   //     commentBody: 'Grass...tastes bad!',
+    //   //     rating: 0
+    //   //   },
+    //   // ]
+    // });
+
+    posts.create({
       title: $scope.title,
-      body: $scope.body,
-      // tags: tagsArray,
-      rating: 0
-      // comments: [
-      //   {
-      //     author: 'Kevin',
-      //     commentBody: 'Hella tight brah!',
-      //     rating: 0
-      //   },
-      //   {
-      //     author: 'Rick',
-      //     commentBody: 'Wubba lubba dub dub!',
-      //     rating: 0
-      //   },
-      //   {
-      //     author: 'Tiny Rick',
-      //     commentBody: 'Grass...tastes bad!',
-      //     rating: 0
-      //   },
-      // ]
+      body: $scope.body
     });
+
     $scope.title = "";
     $scope.body = "";
-    $scope.tags = "";
+    // $scope.tags = "";
   };
 
   $scope.incrementRating = function(post) {
-    post.rating += 1;
+    posts.incrementRating(post);
   };
 
   $scope.decrementRating = function(post) {
-    post.rating -= 1;
+    posts.decrementRating(post);
   };
 
 }]);
 
-app.controller('PostsController', ['$http', '$scope', '$stateParams', 'posts', function($http, $scope, $stateParams, posts){
+app.controller('PostsController', ['$http', '$scope', 'post', 'posts', function($http, $scope, post, posts){
 
-  $scope.post = posts.posts[$stateParams.id];
+  // don't need $stateParams anymore..
+  // $scope.post = posts.posts[$stateParams.id];
+
+  $scope.post = post;
 
   $scope.addComment = function() {
 
@@ -75,20 +86,32 @@ app.controller('PostsController', ['$http', '$scope', '$stateParams', 'posts', f
       return;
     }
 
-    $scope.post.comments.push({
-      author: 'current_user', // this will change later
+    // not doing it this way anymore!
+    // $scope.post.comments.push({
+    //   author: 'current_user', // this will change later
+    //   commentBody: $scope.commentBody,
+    //   rating: 0
+    // });
+
+    // new way:
+    posts.addComment(post.id, {
       commentBody: $scope.commentBody,
-      rating: 0
+      author: 'current_user',
+    }).then(function(comment) {
+      $scope.post.comments.push(comment);
+    }, function(error){
+      console.log("error saving the comment: ", error);
     });
+
     $scope.commentBody = "";
   };
 
   $scope.incrementRating = function(comment) {
-    comment.rating += 1;
+    posts.incrementCommentRating(post, comment);
   };
 
   $scope.decrementRating = function(comment) {
-    comment.rating -= 1;
+    posts.decrementCommentRating(post, comment);
   };
 
 }]);
@@ -103,7 +126,6 @@ app.factory('posts', ['$http', '$scope', function($http, $scope){
 
   this.getAllPosts = function() {
     // didn't work this way (with jbuilders). Trying another way..
-
     // $http.get('/posts').then(function(data) {
     //   console.log("data inside of get /posts is: ", data);
     //   // postObj.posts = data.posts;
@@ -111,11 +133,64 @@ app.factory('posts', ['$http', '$scope', function($http, $scope){
     //   console.log("you had an error: ", error);
     // });
 
-    // second method:
-    return $http.get('/posts.json').success(function(data){
+    return $http.get('/posts.json').then(function(data){
         angular.copy(data, postObj.posts);
+    }, function(error){
+        console.log("you got an error, dummy: ", error);
     });
   }
+
+  this.create = function(post) {
+    return $http.post('/posts.json', post).then(function(data){
+      postObj.posts.push(data);
+    }, function(error){
+        console.log("you got an error, dummy: ", error);
+    });
+  };
+
+  this.incrementRating = function(post) {
+  return $http.put('/posts/' + post.id + '/incrementRating.json').then(function(data){
+      post.rating += 1;
+    }, function(error){
+      console.log("you made another mistake! this time: ", error);
+    });
+  };
+
+  this.decrementRating = function(post) {
+  return $http.put('/posts/' + post.id + '/decrementRating.json').then(function(data){
+      post.rating -= 1;
+    }, function(error){
+      console.log("you made another mistake! this time: ", error);
+    });
+  };
+
+  this.get = function(id) {
+    return $http.get('/posts/' + id + '.json').then(function(res){
+      return res.data;
+    }, function(error){
+      console.log("oh wow another error! you're on a roll: ", error);
+    });
+  };
+
+  this.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments.json', comment);
+  };
+
+  this.inrementCommentRating = function(post, comment) {
+    return $http.put('/posts/' + post.id + '/comments/'+ comment.id + '/incrementCommentRating.json').then(function(data){
+        comment.rating += 1;
+      }, function(error){
+          console.log("error incrementing the comment rating: ", error);
+      });
+  };
+
+  this.decrementCommentRating = function(post, comment) {
+    return $http.put('/posts/' + post.id + '/comments/'+ comment.id + '/decrementCommentRating.json').then(function(data){
+        comment.rating -= 1;
+      }, function(error){
+          console.log("error decrementing the comment rating: ", error);
+      });
+  };
 
 }]);
 
@@ -136,7 +211,12 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
   $stateProvider.state('posts', {
     url: '/posts/:id',
     templateUrl: '/posts.html',
-    controller: 'PostsController'
+    controller: 'PostsController',
+    resolve: {
+      post: ['$stateParams', 'posts', function($stateParams, posts) {
+        return posts.get($stateParams.id);
+      }]
+    }
   });
 
   // getting stuck in an infinite loop with this line..
